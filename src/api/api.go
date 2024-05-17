151e68fc9769592cf779da76301316b8f5baa65d
Psyon <sayan.h2020@gmail.com>
2024-05-17 07:15:53 +0330

    add cfg

--- src/api/api.go
+++ src/api/api.go
@@ -1,11 +1,15 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hailsayan/golang-api/api/routers"
+	"github.com/hailsayan/golang-api/config"
 )
 
-func InitServer(){
+func InitServer() {
+	cfg := config.GetConfig()
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 
@@ -15,5 +19,5 @@ func InitServer(){
 		routers.Health(health)
 	}
 
-	r.Run(":5005")
+	r.Run(fmt.Sprintf(":%s", cfg.Server.Port))
 }
