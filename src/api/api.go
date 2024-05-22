98d57f1f586afbffaa573774afc8a3909f60d8df
Psyon <sayan.h2020@gmail.com>
2024-05-22 21:04:56 +0330

    feat: add custom validation

--- src/api/api.go
+++ src/api/api.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	"github.com/hailsayan/golang-api/api/routers"
+	validation "github.com/hailsayan/golang-api/api/validations"
 	"github.com/hailsayan/golang-api/config"
 )
 
 func InitServer() {
 	cfg := config.GetConfig()
 	r := gin.New()
+
+	val, ok := binding.Validator.Engine().(*validator.Validate)
+	if ok {
+		val.RegisterValidation("mobile", validation.IranianMobileNumberValidator, true)
+	}
+
 	r.Use(gin.Logger(), gin.Recovery())
 
 	api := r.Group("/api")
